8666f5c0e6d2bee324ee4827082a8096ac0e85fe
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-03-20 14:21:22 -0400

    Displays 1st class Functions

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function receivesAFunction(receiveName){
+  receiveName();
+}
+  function returnsANamedFunction(){
+ return function receivesName(){
+ 
+  }
+}
+
+
+function returnsAnAnonymousFunction(){
+ return function(){
+  
+ }
+}
+
+
+
